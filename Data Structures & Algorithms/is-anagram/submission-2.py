8e973fdd7a4abc1d832ce6049794e6960cd3ce12
Caden Nguyen <90945209+dn-cuong@users.noreply.github.com>
2026-05-22 22:54:28 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_Counter = Counter(s)
+        t_Counter = Counter(t)
+        if len(s) != len(t):
+            return False
+        for x in s:
+            if s_Counter[x] != t_Counter[x]:
+                return False
+        return True
