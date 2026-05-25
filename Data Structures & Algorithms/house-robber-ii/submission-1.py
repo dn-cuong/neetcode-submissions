1097749db30dcc2454aac3050b013d1d8871e7c1
Caden Nguyen <90945209+dn-cuong@users.noreply.github.com>
2026-05-25 09:47:50 -0400

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) <= 2:
+            return max(nums)
+
+        dp1 = [0] * (len(nums) - 1)
+        dp2 = [0] * (len(nums) - 1)
+        nums1 = nums[1:]
+        nums2 = nums[:-1]
+
+
+        dp1[0], dp1[1] = nums1[0], max(nums1[0], nums1[1])
+        for i in range(2,len(nums1)):
+            dp1[i] = max(nums1[i] + dp1[i-2], dp1[i-1])
+
+        dp2[0], dp2[1] = nums2[0], max(nums2[0], nums2[1])
+        for i in range(2,len(nums2)):
+            dp2[i] = max(nums2[i] + dp2[i-2], dp2[i-1])
+        
+        return max(dp1[-1], dp2[-1])
