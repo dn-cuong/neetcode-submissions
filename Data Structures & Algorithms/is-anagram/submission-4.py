0e8290ea4dd25c836377abf1568d9e15cf5af3c5
Caden Nguyen <90945209+dn-cuong@users.noreply.github.com>
2026-07-17 10:09:10 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # First solution: Using hashmap to keep track of frequency of each element in two strings
+        # Time complexity: O(Max(s, t))
+        # Space complexity: O(max(s, t))
+
+        if len(s) != len(t):
+            return False
+
+        s_dct = Counter(s)
+        t_dct = Counter(t)
+
+        for x in s_dct:
+            if s_dct[x] != t_dct[x]:
+                return False
+        return True
