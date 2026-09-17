7b009de023cbf767bdb3fc6bdd217a1a0088a24e
Caden Nguyen <90945209+dn-cuong@users.noreply.github.com>
2026-09-17 16:25:20 -0400

    Add: sum-of-two-integers - submission-2

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        a = [int(x) for x in reversed(bin(a)[2:])]
+        b = [int(x) for x in reversed(bin(b)[2:])]
+
+        i = j = 0
+        carry = 0
+        res = []
+
+        while i < len(a) and j < len(b):
+            res.append(a[i] ^ b[j] ^ carry)
+            carry = (a[i] & b[j]) | carry
+            i += 1
+            j += 1
+
+        while i < len(a):
+            res.append(a[i] ^ carry)
+            carry = a[i] & carry
+            i += 1
+
+        while j < len(b):
+            res.append(b[j] ^ carry)
+            carry = b[j] & carry
+            j += 1
+
+        if carry:
+            res.append(carry)
+
+        ans = 0
+        for i in range(len(res)):
+            ans += res[i] * (2 ** i)
+
+        return ans
