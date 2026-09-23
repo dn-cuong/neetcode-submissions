af38a8c22f0ba94859eabf56cd71dcae959d28c0
Caden Nguyen <90945209+dn-cuong@users.noreply.github.com>
2026-09-23 14:43:45 -0400

    Add: n-ary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/n-ary-tree-postorder-traversal/submission-0.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        ans = []
+        def dfs(root):
+            if not root:
+                return None
+            for i in range(len(root.children)):
+                dfs(root.children[i])
+                ans.append(root.children[i].val)
+
+
+        dfs(root)
+
+        return ans + [root.val]
